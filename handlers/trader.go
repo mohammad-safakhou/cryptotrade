96311180a65572e91772560e2b241f72c3562c56
Mohammad Safakhou <mnim@Mohammads-MacBook-Pro.local>
2022-03-09 18:33:07 +0330

    trader handler

--- handlers/trader.go
+++ handlers/trader.go
@@ -1,28 +1,56 @@
 package handlers
 
-import "context"
+import (
+	"context"
+	"cryptotrade/pkg"
+	"log"
+)
 
 type Trader interface {
+	Handler(ctx context.Context) error
 	Buy(ctx context.Context) error
 	Sell(ctx context.Context) error
 	Exit(ctx context.Context) error
 }
 
 type traderHandler struct {
+	MessageBrokerCandleHandler pkg.MessageBrokerHandler
 }
 
 func NewTraderHandler() Trader {
 	return &traderHandler{}
 }
 
-func (t traderHandler) Buy(ctx context.Context) error {
+func (t *traderHandler) Handler(ctx context.Context) error {
+	dataChan := make(chan []byte)
+	t.MessageBrokerCandleHandler.Consumer(ctx, dataChan)
+	for data := range dataChan {
+		switch string(data) {
+		case "BUY":
+			err := t.Buy(ctx)
+			if err != nil {
+				log.Printf("error on calling buy method in trader: %s", err.Error())
+			}
+		case "SELL":
+			err := t.Sell(ctx)
+			if err != nil {
+				log.Printf("error on calling sell method in trader: %s", err.Error())
+			}
+		default:
+			continue
+		}
+	}
+	return nil
+}
+
+func (t *traderHandler) Buy(ctx context.Context) error {
 	panic("implement me")
 }
 
-func (t traderHandler) Sell(ctx context.Context) error {
+func (t *traderHandler) Sell(ctx context.Context) error {
 	panic("implement me")
 }
 
-func (t traderHandler) Exit(ctx context.Context) error {
+func (t *traderHandler) Exit(ctx context.Context) error {
 	panic("implement me")
 }

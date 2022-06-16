5605d69d57796309b031e7dd72f4561b6c0355f6
mohammad.safakhou <mnim220@yahoo.com>
2022-06-16 15:47:47 +0430

    receiver webhook

--- cmd/receiver.go
+++ cmd/receiver.go
@@ -2,13 +2,15 @@ package cmd
 
 import (
 	"context"
-	"cryptotrade/handlers"
-	"cryptotrade/pkg"
-	"cryptotrade/repository"
-	"cryptotrade/utils"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/spf13/cobra"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"time"
 )
 
 func init() {
@@ -19,26 +21,68 @@ var receiverCmd = &cobra.Command{
 	Use:   "receiver",
 	Short: "Starting candle receiver",
 	Run: func(cmd *cobra.Command, args []string) {
-		dbPostgres, err := utils.PostgresConnection("localhost", "5432", "root", "root", "cryptotrade", "disable")
-		if err != nil {
-			panic(err)
-		}
+		e := echo.New()
 
-		candleKafka, err := pkg.KafkaConnection("127.0.0.1", "9092", "candles", 0)
-		if err != nil {
-			panic(err)
-		}
-		candleKafkaHandler := pkg.NewKafkaHandler(candleKafka)
-		candlesRepository := repository.NewCandlesRepository(dbPostgres)
+		// Middleware
+		e.Use(middleware.Logger())
+		e.Use(middleware.Recover())
+		e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+			AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+		}))
 
-		receiverHandler := handlers.NewReceiverHandler(nil, "1min", 30, candlesRepository, candleKafkaHandler)
+		// Routes
+		e.POST("/receiver", func(ctx echo.Context) error {
+			type receiverModel struct {
+				Text string `json:"text"`
+			}
+			req := new(receiverModel)
+			if err := ctx.Bind(req); err != nil {
+				return ctx.JSON(http.StatusBadRequest, err)
+			}
 
+			fmt.Println(req)
+			return ctx.JSON(http.StatusOK, "")
+		})
+
+		// Start server
+		go func() {
+			if err := e.Start(":10000"); err != nil && err != http.ErrServerClosed {
+				log.Println(err)
+				log.Fatal("shutting down the server")
+			}
+		}()
+
+		// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
 		quit := make(chan os.Signal, 1)
 		signal.Notify(quit, os.Interrupt)
-
-		go func(receiverHandler handlers.Receiver) {
-			receiverHandler.CallRepeater(context.Background(), "BTCUSDT")
-		}(receiverHandler)
 		<-quit
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := e.Shutdown(ctx); err != nil {
+			e.Logger.Fatal(err)
+		}
+
+		//dbPostgres, err := utils.PostgresConnection("localhost", "5432", "root", "root", "cryptotrade", "disable")
+		//if err != nil {
+		//	panic(err)
+		//}
+		//
+		//candleKafka, err := pkg.KafkaConnection("127.0.0.1", "9092", "candles", 0)
+		//if err != nil {
+		//	panic(err)
+		//}
+		//candleKafkaHandler := pkg.NewKafkaHandler(candleKafka)
+		//candlesRepository := repository.NewCandlesRepository(dbPostgres)
+		//
+		//receiverHandler := handlers.NewReceiverHandler(nil, "1min", 30, candlesRepository, candleKafkaHandler)
+		//
+		//quit := make(chan os.Signal, 1)
+		//signal.Notify(quit, os.Interrupt)
+		//
+		//go func(receiverHandler handlers.Receiver) {
+		//	receiverHandler.CallRepeater(context.Background(), "BTCUSDT")
+		//}(receiverHandler)
+		//<-quit
 	},
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/friendsofgo/errors v0.9.2
 	github.com/go-redis/redis/v8 v8.11.4
 	github.com/gorilla/websocket v1.5.0
+	github.com/labstack/echo/v4 v4.7.2
 	github.com/lib/pq v1.10.4
 	github.com/segmentio/kafka-go v0.4.25
 	github.com/spf13/cobra v1.2.1
@@ -21,11 +22,15 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/klauspost/compress v1.9.8 // indirect
+	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
@@ -34,6 +39,8 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
 	github.com/volatiletech/inflect v0.0.1 // indirect
 	github.com/volatiletech/randomize v0.0.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
@@ -42,6 +49,7 @@ require (
 	golang.org/x/net v0.0.0-20211118161319-6a13c67c3ce4 // indirect
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gopkg.in/ini.v1 v1.64.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect

b1708df322bc1a53f29c43a9f3da4bd730bfd9e4
mohammad.safakhou <mnim220@yahoo.com>
2022-07-12 22:18:39 +0430

    with stop

--- script/main.go
+++ script/main.go
@@ -36,13 +36,24 @@ type Records struct {
 	Sell   int       `json:"sell"`
 }
 
-func main() {
-	freshRecords := readCsvFile("/home/mohammad/repos/mine/cryptotrade/script/1.csv")[1:]
-	freshRecords = append(freshRecords, readCsvFile("/home/mohammad/repos/mine/cryptotrade/script/2.csv")[1:]...)
+var Kir = 0
+
+func Calculate(name string) (string, float64) {
+	freshRecords := readCsvFile("/home/mohammad/repos/mine/cryptotrade/script/BINANCE_BTCUSDTPERP, " + name + ".csv")[1:]
+	//freshRecords = append(freshRecords, readCsvFile("/home/mohammad/repos/mine/cryptotrade/script/2.csv")[1:]...)
 
 	var records []Records
 	for _, freshRecord := range freshRecords {
 		recordTime, _ := time.Parse("2006-01-02T15:04:05Z", freshRecord[0])
+		//if recordTime.Year() < 2022 {
+		//	continue
+		//}
+		//if recordTime.Month() < time.Now().Month() - 1 {
+		//	continue
+		//}
+		if time.Now().Sub(recordTime).Hours() > 30*24 {
+			continue
+		}
 		recordOpen, _ := strconv.ParseFloat(freshRecord[1], 10)
 		recordHigh, _ := strconv.ParseFloat(freshRecord[2], 10)
 		recordLow, _ := strconv.ParseFloat(freshRecord[3], 10)
@@ -82,9 +93,30 @@ func main() {
 
 	records = records[startIndex+1:]
 
+	Kir = 0
+	var currentStatus int
+	if prevRecord.Buy == 1 {
+		currentStatus = 1
+	} else if prevRecord.Sell == 1 {
+		currentStatus = -1
+	}
 	var outcome float64
 	for _, record := range records {
+		if currentStatus == 0 {
+			if record.Buy == 0 && record.Sell == 0 {
+				continue
+			} else {
+				prevRecord = record
+				if record.Buy == 1 {
+					currentStatus = 1
+				} else if record.Sell == 1 {
+					currentStatus = -1
+				}
+				continue
+			}
+		}
 		if record.Buy == 1 && record.Sell == 1 {
+			Kir += 1
 			if prevRecord.Buy == 1 {
 				record.Buy = 0
 			} else {
@@ -92,27 +124,90 @@ func main() {
 			}
 		}
 
-		diff := record.Close - prevRecord.Close
 		if prevRecord.Buy == 1 {
-			if diff > 0 {
-				// taking profit
-				outcome = outcome + diff
-			} else {
-				// losing profit
-				outcome = outcome + diff
+			if record.Low < prevRecord.Low {
+				diff := record.Close - prevRecord.Close
+				if prevRecord.Buy == 1 {
+					if diff > 0 {
+						// taking profit
+						outcome = outcome + diff
+					} else {
+						// losing profit
+						outcome = outcome + diff
+					}
+				} else {
+					if diff > 0 {
+						// losing profit
+						outcome = outcome - diff
+					} else {
+						// taking profit
+						outcome = outcome - diff
+					}
+				}
+				currentStatus = 0
 			}
 		} else {
-			if diff > 0 {
-				// losing profit
-				outcome = outcome - diff
-			} else {
-				// taking profit
-				outcome = outcome - diff
+			if record.High > prevRecord.High {
+				diff := record.Close - prevRecord.Close
+				if prevRecord.Buy == 1 {
+					if diff > 0 {
+						// taking profit
+						outcome = outcome + diff
+					} else {
+						// losing profit
+						outcome = outcome + diff
+					}
+				} else {
+					if diff > 0 {
+						// losing profit
+						outcome = outcome - diff
+					} else {
+						// taking profit
+						outcome = outcome - diff
+					}
+				}
+				currentStatus = 0
 			}
 		}
 
-		prevRecord = record
+		if record.Buy == 1 || record.Sell == 1 {
+			diff := record.Close - prevRecord.Close
+			if prevRecord.Buy == 1 {
+				if diff > 0 {
+					// taking profit
+					outcome = outcome + diff
+				} else {
+					// losing profit
+					outcome = outcome + diff
+				}
+			} else {
+				if diff > 0 {
+					// losing profit
+					outcome = outcome - diff
+				} else {
+					// taking profit
+					outcome = outcome - diff
+				}
+			}
+			prevRecord = record
+		}
+
 	}
 
-	fmt.Println(outcome)
+	//fmt.Println(Kir)
+	return name, outcome
+}
+func main() {
+	fmt.Println(Calculate("1"))
+	fmt.Println(Calculate("3"))
+	fmt.Println(Calculate("5"))
+	fmt.Println(Calculate("15"))
+	fmt.Println(Calculate("30"))
+	fmt.Println(Calculate("60"))
+	fmt.Println(Calculate("120"))
+	fmt.Println(Calculate("180"))
+	fmt.Println(Calculate("240"))
+	fmt.Println(Calculate("1D"))
+	fmt.Println(Calculate("1W"))
+	fmt.Println(Calculate("1M"))
 }
